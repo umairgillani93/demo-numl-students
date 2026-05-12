66c65d662f3e35223445c6e014d2ea1b5b09da30
umairgillani93 <umairgillani93@gmail.com>
2026-05-12 11:15:11 +0500

    added feature subtract in feature/subtact

--- main.cpp
+++ main.cpp
@@ -7,16 +7,17 @@ int add(int a, int b) {
 	return z;
 }
 
-//int sub(int a, int b) {
-//	int z = a - b;
-//	return z;
-//}
+int sub(int a, int b) {
+	int z = a - b;
+	return z;
+}
 
 int main() {
 	int a = 10;
 	int b = 5;
 	int add_res = add(a, b);
-	cout << add_res << '\n';
+	int sub_res = sub(a, b);
+	cout << sub_res << '\n';
 	return 0;
 	
 }
