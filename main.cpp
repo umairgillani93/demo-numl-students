81a1c9bfae7e4b4002af91f25c13d6b107efbfdf
umairgillani93 <umairgillani93@gmail.com>
2026-05-12 11:11:07 +0500

    written add method of the calculator

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int add(int a, int b) {
+	int z = a + b;
+	return z;
+}
+
+//int sub(int a, int b) {
+//	int z = a - b;
+//	return z;
+//}
+
+int main() {
+	int a = 10;
+	int b = 5;
+	int add_res = add(a, b);
+	cout << add_res << '\n';
+	return 0;
+	
+}
